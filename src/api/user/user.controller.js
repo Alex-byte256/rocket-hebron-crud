d0a086db0a0f6af96b4a20462e42546456cf1ee6
Alex-byte256 <oleksandr.kozak.2019@gmail.com>
2022-12-22 01:38:33 +0300

    add post user

--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -1,15 +1,23 @@
-const users = require("../../dataBase/users");
+const users = require("../../dataBase/users.json");
 const userService = require("./user.service");
+const fs =  require("node:fs").promises;
+const path =require("node:path")
+
+const currentDir = path.join(__dirname,"../../dataBase/");
+const dataBaseFile = path.join(currentDir,"users.json");
 
 module.exports = {
     getAllUsers: (req, res) => {
         res.json(users);
     },
 
-    createUser: (req, res) => {
+    createUser: async (req, res) => {
         try {
             console.log(req.body);
-
+            let el = await  fs.readFile(dataBaseFile)
+            let newUs = JSON.parse(el);
+            newUs.push(req.body)
+            await fs.writeFile(dataBaseFile,JSON.stringify(newUs))
             res.status(201).json('HELLO TEST CHAT');
         } catch (e) {
             res.status(400).json(e.message);
--- src/dataBase/users.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-    { name: 'Ruslan', age: '20' },
-    { name: 'Pchel', age: '26' },
-    { name: 'Taras', age: '21' },
-    { name: 'Julia', age: '19' }
-]
