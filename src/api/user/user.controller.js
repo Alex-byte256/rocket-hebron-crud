24aa5eb1db47b6861200e2c963acce030f10b1b0
Alex-byte256 <oleksandr.kozak.2019@gmail.com>
2022-12-23 02:52:43 +0300

    fix

--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -1,10 +1,6 @@
 const users = require("../../dataBase/users.json");
 const userService = require("./user.service");
-const fs =  require("node:fs").promises;
-const path =require("node:path")
 
-const currentDir = path.join(__dirname,"../../dataBase/");
-const dataBaseFile = path.join(currentDir,"users.json");
 
 module.exports = {
     getAllUsers: (req, res) => {
@@ -14,10 +10,7 @@ module.exports = {
     createUser: async (req, res) => {
         try {
             console.log(req.body);
-            let allUs = await  fs.readFile(dataBaseFile)
-            let newUs = JSON.parse(allUs);
-            newUs.push(req.body)
-            await fs.writeFile(dataBaseFile,JSON.stringify(newUs))
+            await userService.postUser(req.body);
             res.status(201).json('user created');
         } catch (e) {
             res.status(400).json(e.message);
@@ -27,9 +20,7 @@ module.exports = {
     getUserById: async (req, res) => {
         try {
             console.log(req.params);
-
             const user = await userService.getSingleUser(req.params.userId);
-
             res.json(user);
         } catch (e) {
             res.status(400).json(e.message);
@@ -38,18 +29,10 @@ module.exports = {
 
     updateUser: async (req, res) => {
         try {
-            const id = req.params.userId - 1;
-            if(users[req.params.userId - 1] === undefined){
-                throw new Error("no user with this id")
-                return;
-            }
-            let allUs = await  fs.readFile(dataBaseFile);
-            let updUs = JSON.parse(allUs);
-            updUs.splice(id,1,req.body)
-            await fs.writeFile(dataBaseFile,JSON.stringify(updUs))
+            await  userService.updUser(req.params.userId,req.body)
             res.json('update user');
         }catch (e) {
-            console.log(e);
+            res.json(e);
         }
 
 
@@ -58,22 +41,12 @@ module.exports = {
     deleteUser: async (req, res) => {
         try {
             console.log(req.params.userId);
-            const id = req.params.userId - 1;
-            if(users[req.params.userId - 1] === undefined){
-                throw new Error("no user with this id")
-                return;
-            }
-            let allUs = await  fs.readFile(dataBaseFile);
-            let delUs = JSON.parse(allUs);
-            delUs.splice(id,1)
-            await fs.writeFile(dataBaseFile,JSON.stringify(delUs))
+            await userService.dltUser(req.params.userId)
             res.json('delete user');
             res.status(204).json('User was deleted');
         }catch (e){
-            console.log(e);
+            res.json(e);
         }
-
-
 
     }
 }
--- src/api/user/user.service.js
+++ src/api/user/user.service.js
@@ -1,4 +1,11 @@
 const users = require("../../dataBase/users");
+const {promises: fs} = require("node:fs");
+const path = require("node:path");
+
+const currentDir = path.join(__dirname,"../../dataBase/");
+const dataBaseFile = path.join(currentDir,"users.json");
+
+
 module.exports = {
     getSingleUser: async (userId) => {
         const user = users[userId - 1];
@@ -8,5 +15,33 @@ module.exports = {
         }
 
         return user;
+    },
+    postUser: async (reqBody)=>{
+        let allUs = await  fs.readFile(dataBaseFile)
+        let newUs = JSON.parse(allUs);
+        newUs.push(reqBody)
+        await fs.writeFile(dataBaseFile,JSON.stringify(newUs))
+    },
+    updUser: async  (userId,body) => {
+        const id = userId - 1;
+        if(users[userId - 1] === undefined){
+            throw new Error("no user with this id")
+            return;
+        }
+        let allUs = await  fs.readFile(dataBaseFile);
+        let updUs = JSON.parse(allUs);
+        updUs.splice(id,1,body)
+        await fs.writeFile(dataBaseFile,JSON.stringify(updUs))
+},
+    dltUser: async  (userId)=> {
+        const id = userId - 1;
+        if(users[userId - 1] === undefined){
+            throw new Error("no user with this id")
+            return;
+        }
+        let allUs = await  fs.readFile(dataBaseFile);
+        let delUs = JSON.parse(allUs);
+        delUs.splice(id,1)
+        await fs.writeFile(dataBaseFile,JSON.stringify(delUs))
     }
 }
